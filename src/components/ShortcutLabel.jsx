cb75557e268e45fddf8d4ce649c91ec82940c152
Ben <benjamman@proton.me>
2025-07-16 06:39:28 -0400

    Don't create empty shortcuts

--- src/components/ShortcutLabel.jsx
+++ src/components/ShortcutLabel.jsx
@@ -1,21 +1,22 @@
 function ShortcutLabel({ mod, bind, variant = "background1" }) {
-  return (
-    <>
-      <span is-="badge" variant-={variant} className="shortcut-label">
-        [
-        {mod ? (
-          <>
-            <span data-mod-key={mod}>{mod}</span>{" "}
-            <span style={{ paddingInline: "1ch" }}>+</span>{" "}
-            <span data-bind-key={bind}>{bind}</span>
-          </>
-        ) : (
-          <span data-bind-key={bind}>{bind}</span>
-        )}
-        ]
-      </span>
-    </>
-  );
+    if (!bind) return <></>
+    return (
+        <>
+            <span is-="badge" variant-={variant} className="shortcut-label">
+                [
+                {mod ? (
+                    <>
+                        <span data-mod-key={mod}>{mod}</span>{" "}
+                        <span style={{ paddingInline: "1ch" }}>+</span>{" "}
+                        <span data-bind-key={bind}>{bind}</span>
+                    </>
+                ) : (
+                    <span data-bind-key={bind}>{bind}</span>
+                )}
+                ]
+            </span>
+        </>
+    );
 }
 
 export default ShortcutLabel;
